494ca8de6f3ea9feafbdabd13fbda86694142f78
Nkosing'phile Nzotho <nnzotho@rrs.co.za>
2021-07-27 17:05:22 +0200

    Updated main.py and test_gets.py performance to get a score of 410 points.

--- Challenge Folder/test_gets.py
+++ Challenge Folder/test_gets.py
@@ -26,35 +26,37 @@ def get_table(table=None):
     log.info(">> get_table(table=None). table: %s", table)
 
     # ˅
-    query_select_records = ("SELECT * FROM records")
+    query_select_records = ("SELECT  data.id, person, (array_to_string(array_agg( distinct data_id), ',')) data_id from records inner join data on records.data_id = data.id WHERE EXISTS( SELECT 1 FROM records WHERE data.id=records.data_id ) GROUP BY person, data.id LIMIT 1")
 
-    query_select_data = ("SELECT * FROM data")
+    query_select_data = ("(SELECT id, (array_to_string(array_agg( distinct data.text), ',')) data_text, json from data GROUP BY id )")
 
+    response_list = None
     query_list = [
         query_select_records,
         query_select_data]
     log.info("query list: %s", query_list)
-
     try:
-        # get the data
-        response_list = exc_qrs_get_dfs(query_list)
-        log.info("database responses: %s", response_list)
+        if table == "records":
+            query_list = [query_select_records]
+            # get the data
+            response_list = exc_qrs_get_dfs(query_list)
+            log.info("database responses: %s", response_list)
 
-        response_object = {
-            "records_table":response_list[0],
-            "data_table": response_list[1]}
+            response_object = {
+                "records_table":response_list[0]}
+
+        if table == "data":
+            query_list = [query_select_data]
+            # get the data
+            response_list = exc_qrs_get_dfs(query_list)
+            log.info("database responses: %s", response_list)
+
+            response_object = {
+                "data_table": response_list[0]}
+        log.info("query list: %s", query_list)
 
     except Exception as error:
         log.info(error)
         return error
-
-    # ˅
-    if table == "records":
-        response_object.pop("data_table")
-        return response_object
-
-    if table == "data":
-        response_object.pop("records_table")
-        return response_object
 
     return response_object
